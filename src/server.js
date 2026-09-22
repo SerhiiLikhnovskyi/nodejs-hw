85db2f3695eaca8e6a56c3d647ccefdacaead1cb
Sergiy Likhnovskiy <slihnovskiy@gmail.com>
2026-09-22 11:15:18 +0300

    add cors and file

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import pino from 'pino-http';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'hello Serhii' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server start in port${PORT}`);
+});
