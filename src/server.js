7c7fc235cc9b6286219900c4cc7618d5adff9da3
Sergiy Likhnovskiy <slihnovskiy@gmail.com>
2026-09-22 12:20:23 +0300

    added all middleware

--- src/server.js
+++ src/server.js
@@ -25,8 +25,26 @@ app.use(
     },
   }),
 );
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'hello Serhii' });
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.get('/test-error', (req, res, next) => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
 });
 
 app.listen(PORT, () => {
